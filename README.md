a99b5d0a80888fefbea369e2a880faa6fda02053
Joseph Greenfield <joseph.greenfield@parsons.com>
2021-09-05 11:18:34 -0400

    Added new paths with diffirent types of routes. You can type routes.

--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # flask-server
 
-### 1) Install dependencies to virtual enviornment
+# 1) Install dependencies to virtual enviornment
 virtualenv .env && source .env/bin/activate && pip install -r requirements.txt
 
-### 2) Run Flask App
+# 2) Run Flask App
 flask run
+## If you would like to make the server availible to your network, use:
+flask run --host=0.0.0.0
+
+## To enable dev features, set the FLASK_ENV enviornment variable to development:
+export FLASK_ENV=development
--- app.py
+++ app.py
@@ -1,8 +1,41 @@
 from flask import Flask
+from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
-    
+
+
+###################################################
+# You can specify types in the path:
+# string - (default) accepts any text without a slash
+# int - accepts positive integers
+# float - accepts positive floating point values
+# path - like string but also accepts slashes
+# uuid - accepts UUID strings
+###################################################
+
+# Use escape for user provided values to protect from injection attacks
+@app.route("/<name>")
+def hello(name):
+    return f"Hello, {escape(name)}!"
+
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return f'User {escape(username)}'
+
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return f'Post {post_id}'
+
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return f'Subpath {escape(subpath)}'
